af3129483ce90a23a49002474e8b266974e2c187
Dimo Yanev <dyanev@kpmg.com>
2024-10-14 18:52:14 +0300

    add exam prep3

--- src/03_Advanced/08_Exam_Prep/Еxam_prep_2/01_Wild_Survival.py
+++ src/03_Advanced/08_Exam_Prep/Еxam_prep_2/01_Wild_Survival.py
@@ -1,10 +1,26 @@
+import math
 from collections import deque
 
 bees_queue = deque([int(el) for el in input().split()])
 bee_eaters_stack = [int(el) for el in input().split()]
 
 while bees_queue and bee_eaters_stack:
-    current_bee = bees_queue[0]
-    current_eater = bee_eaters_stack[-1]
+    current_bee = bees_queue.popleft()
+    current_eater = bee_eaters_stack.pop()
 
-    print()
+    eater_power = current_eater * 7
+
+    if eater_power < current_bee:
+        bees_queue.append(current_bee - eater_power)
+    elif current_bee > eater_power:
+        bee_eaters_stack.append(math.ceil(eater_power - current_bee // 7))
+    else:
+        continue
+
+print("The final battle is over!")
+if not bees_queue and not bee_eaters_stack:
+    print("But no one made it out alive!")
+if bees_queue:
+    print(f"Bee groups left: {', '.join(map(str, bees_queue))}")
+if bee_eaters_stack:
+    print(f"Bee-eater groups left: {', '.join(map(str, bee_eaters_stack))}")
